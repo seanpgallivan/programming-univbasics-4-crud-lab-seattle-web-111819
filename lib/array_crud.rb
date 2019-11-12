0442609ec9a9ff347142a491edb08eb67e2944cf
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-12 05:08:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,15 +23,15 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[array.leng]
 end
 
 def update_element_from_index(array, index_number, element)
